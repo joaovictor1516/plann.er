c4179d33486cee242ea893b104165911fed743d4
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-07-24 01:54:45 -0300

    desenvolvimento da tela de confirmacao do envio dos convirtes da viagem.

--- frontEnd/src/app.tsx
+++ frontEnd/src/app.tsx
@@ -1,5 +1,5 @@
-import { ArrowRight, MapPin, Calendar, UserRoundPlus, Settings2, Plus, AtSign, X } from "lucide-react";
-import { FormEvent, useState } from "react";
+import { ArrowRight, MapPin, Calendar, UserRoundPlus, Settings2, Plus, AtSign, X, Mail, User } from "lucide-react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import { toast } from "sonner";
 
 export function App(){
@@ -7,7 +7,10 @@ export function App(){
   const [isGuestsModalOpen, setIsGuestsModalOpen] = useState<boolean>(false);
   const [IsEndConfigTravel, setIsEndConfigTravel] = useState<boolean>(false);
   const [isEmailExist, setIsEmailExist] = useState<boolean>(false);
+
   const [emailsToInvite, setEmailsToInvite] = useState<string[]>([]);
+  const [locationInput, setLocationInput] = useState<string>("");
+  const [dateInput, setDateInput] = useState<string>("");
   function openGuestsInput(){
     setIsGuestsInputOpen(true);
   }
@@ -81,6 +84,22 @@ export function App(){
     setIsEndConfigTravel(false);
   }
 
+  function handleLocation(element: ChangeEvent<HTMLInputElement>){
+    const location = element.target.value;
+
+    if(location !== ""){
+      setLocationInput(location);
+    }
+  }
+
+  function handleDate(element: ChangeEvent<HTMLInputElement>){
+    const date = element.target.value;
+
+    if(date !== ""){
+      setDateInput(date);
+    }
+  }
+
   return(
     <div className="h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center">
       <div className="max-w-3xl w-full text-center space-y-10">
@@ -95,9 +114,10 @@ export function App(){
                 <MapPin className="size-5 text-zinc-400"/>
                 <input type="text"
                       name="locationInput"
-                      id="locationInpu"
+                      id="locationInput"
                       disabled={isGuestsInputOpen}
                       className="bg-transparent placeholder-zinc-400 text-lg outline-none"
+                      onChange={handleLocation}
                       placeholder="Para onde você vai?"/>
               </div>
               <div className="flex items-center gap-2">
@@ -107,6 +127,7 @@ export function App(){
                       id="dateInput"
                       disabled={isGuestsInputOpen}
                       className="bg-transparent placeholder-zinc-400 text-lg outline-none"
+                      onChange={handleDate}
                       placeholder="Quando?"/>
               </div>
             <div className="w-px h-6 bg-zinc-800"/>
@@ -197,15 +218,48 @@ export function App(){
         )}
 
         {IsEndConfigTravel && (
-          <div className="fixed inset-0 bg-black/60">
-            <div className="flex flex-col ">
-              <div className="flex justify-between">
+          <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+            <div className="flex flex-col items-center bg-zinc-900 w-[640px] rounded-xl text-zinc-400 px-6 py-5 gap-5 shadow-shape">
+              <div className="flex flex-col justify-center w-full gap-2">
+                <div className="flex justify-between">
+                  <h2 className="text-zinc-50 text-lg font-semibold">
+                    Confirmar criação da viagem.
+                  </h2>
+                  <button>
+                    <X className="size-5" onClick={disconfirmTravel}/>
+                  </button>
+                </div>
 
-                <button className="" onClick={disconfirmTravel}>
-                  <X />
-                </button>
+                <p className="text-left text-sm">
+                Para concluir a criação da viagem para <span className="text-zinc-100"> {locationInput} </span> nas datas <span className="text-zinc-100"> {dateInput} </span> preencha seus dados abaixo:
+                </p>
               </div>
-              <div className=""></div>
+              
+              <form className="flex flex-col items-center justify-between border w-full border-none gap-4">
+                <div className="flex flex-col items-center gap-2 w-full">
+                  <div className="flex items-center gap-2 w-full bg-zinc-950 px-3 py-2 rounded-lg">
+                    <User className="size-5 text-zinc-400"/>
+                    <input type="email"
+                          name="emailInput"
+                          id="emailInput"
+                          className="bg-transparent placeholder-zinc-400 text-lg outline-none w-full"
+                          placeholder="Digite o seu nome completo"/>
+                  </div>
+
+                  <div className="flex items-center gap-2 w-full bg-zinc-950 px-3 py-2 rounded-lg">
+                    <Mail className="size-5 text-zinc-400"/>
+                    <input type="email"
+                          name="emailInput"
+                          id="emailInput"
+                          className="bg-transparent placeholder-zinc-400 text-lg outline-none w-full"
+                          placeholder="Digite o seu e-mail"/>
+                  </div>
+                </div>
+
+                <button type="submit" className="bg-lime-300 text-lime-950 flex items-center text-center px-5 py-2 gap-2 rounded-lg font-medium hover:bg-lime-400 w-full">
+                  Confirmar criação da vigem.
+                </button>
+              </form>
             </div>
           </div>
         )}
