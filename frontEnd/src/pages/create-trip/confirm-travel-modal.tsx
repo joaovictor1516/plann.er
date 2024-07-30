d27624433cbf732d1d2b84484de7235cad7c3a61
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-07-30 13:40:01 -0300

    refatoracao, melhoria na aparencia do codigo.

--- frontEnd/src/pages/create-trip/confirm-travel-modal.tsx
+++ frontEnd/src/pages/create-trip/confirm-travel-modal.tsx
@@ -4,7 +4,6 @@ interface ConfirmTravelModalType{
     locationInput: string;
     dateInput: string;
     disconfirmTravel: () => void;
-
 }
 
 export function ConfirmTravelModal(props: ConfirmTravelModalType){
