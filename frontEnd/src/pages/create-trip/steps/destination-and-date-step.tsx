4ba698c0d8a4a71162903e2dc7905b0595722fde
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-08-06 12:14:47 -0300

    remocao de codigo css redundante.

--- frontEnd/src/pages/create-trip/steps/destination-and-date-step.tsx
+++ frontEnd/src/pages/create-trip/steps/destination-and-date-step.tsx
@@ -34,12 +34,12 @@ export function DestinationAndDateStep(props: DestinationAndDateType){
             </div>
             <div className="w-px h-6 bg-zinc-800"/>
             {props.isGuestsInputOpen ? (
-                <button className="bg-zinc-800 text-zinc-200 flex items-center text-center px-5 py-2 gap-2 rounded-lg font-medium hover:bg-zinc-700 w-56" onClick={props.closeGuestInput}>
+                <button className="bg-zinc-800 text-zinc-200 flex items-center px-5 py-2 gap-2 rounded-lg font-medium hover:bg-zinc-700 w-56" onClick={props.closeGuestInput}>
                   Alterar local/data
                   <Settings2 className="size-5"/>
                 </button>
               ):(
-                <button className="bg-lime-300 text-lime-950 flex items-center text-center px-5 py-2 gap-2 rounded-lg font-medium hover:bg-lime-400" onClick={props.openGuestsInput}>
+                <button className="bg-lime-300 text-lime-950 flex items-center px-5 py-2 gap-2 rounded-lg font-medium hover:bg-lime-400" onClick={props.openGuestsInput}>
                   Continuar
                   <ArrowRight className="size-5"/>
                 </button>
