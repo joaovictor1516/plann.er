8957b6e2939a15d41821d46f064f06f358aa32b4
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-08-06 15:18:55 -0300

    desenvolvimento da parate de informacao do local e data da viagem e dos eventos que irao ocorrer.

--- frontEnd/src/pages/trip-details/index.tsx
+++ frontEnd/src/pages/trip-details/index.tsx
@@ -3,40 +3,52 @@ import { MapPin, Calendar, Plus, Settings2 } from "lucide-react";
 export function TripDetailsPage(){
     
     return(
-        <div className="flex flex-col items-center justify-center bg-zinc-950 w-screen py-10 gap-8">
-            <div className="flex gap-5 bg-zinc-900 w-[1100px] pl-6 pr-4 h-16 items-center justify-center rounded-xl">
-                <div className="flex flex-1 items-center gap-2">
-                        <MapPin className="size-5"/>
-                        <input type="text" className="bg-transparent"/>
+        <div className="max-w-6xl px-6 py-10 mx-auto space-y-8">
+            <div className="flex items-center justify-between bg-zinc-900 px-4 h-16 rounded-xl shadow-shape">
+                <div className="flex items-center gap-2">
+                    <MapPin className="size-5 text-zinc-400"/>
+                    <span className="text-zinc-100">
+                        Rio de Janeiro, Brasil
+                    </span>
                 </div>
-                <div className="flex flex-1 items-center gap-2">
-                        <Calendar className="size-5"/>
-                        <input type="text" className="bg-transparent "/>
+                <div className="flex items-center gap-5">
+                    <Calendar className="size-5 text-zinc-400"/>
+                    <span className="text-zinc-100">
+                        17 a 23 de Agosto
+                    </span>
+                    <div className="w-px h-6 bg-zinc-800"/>
+                    <button className="bg-zinc-800 text-zinc-200 flex items-center gap-2 px-5 py-2 rounded-lg font-medium hover:bg-zinc-700">
+                        Alterar local/data
+                        <Settings2 className="size-5"/>
+                    </button>
                 </div>
-                <button className="bg-zinc-800 text-zinc-200 flex justify-center text-center px-5 py-2 gap-2 rounded-lg font-medium hover:bg-zinc-700 w-56">
-                    Alterar local/data
-                    <Settings2 className="size-5"/>
-                </button>
             </div>
-            
-            <div className="flex">
-                <div className="flex flex-col self-start">
-                    <div className="flex items-center justify-between w-max">
-                        <span className="">Atividades</span>
 
-                        <button className="bg-lime-300 text-lime-950 flex items-center justify-center gap-2 px-5 py-2 rounded-lg font-medium">
+            <main className="flex gap-16 px-6">
+                <div className="flex-1 space-y-6">
+                    <div className="flex items-center justify-between">
+                        <h2 className="text-3xl font-semibold">
+                            Atividades
+                        </h2>
+                        <button className="bg-lime-300 text-lime-950 flex items-center gap-2 px-5 py-2 rounded-lg font-medium hover:bg-lime-400">
                             <Plus className="size-5"/>
                             Cadastrar atividade
                         </button>
                     </div>
-                    <div className="">
 
+                    <div className="space-y-8">
+                        <div className="space-y-3">
+                            <div className="flex gap-2 items-baseline">
+                                <span className="text-zinc-300 text-xl font-semibold">Dia 17</span>
+                                <span className="text-zinc-500 text-xs">Sábado</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className="">
+                <div className="w-80 space-y-6">
 
                 </div>
-            </div>
+            </main>
         </div>
     )
 }
