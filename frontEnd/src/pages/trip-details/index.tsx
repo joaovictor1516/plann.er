a5c1701e0148b6949e4c477d0032bb81f8efe217
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-08-08 13:54:52 -0300

    adicao do componente de criacao de atividade e do componente responsavel por informar o local e as datas da viagem.

--- frontEnd/src/pages/trip-details/index.tsx
+++ frontEnd/src/pages/trip-details/index.tsx
@@ -1,5 +1,7 @@
-import { MapPin, Calendar, Plus, Settings2, CircleCheck, Link2, CircleDashed, UserCog, X, Tag, Clock, User, Mail } from "lucide-react";
+import { Plus, CircleCheck, Link2, CircleDashed, UserCog, X, Tag, User, Mail } from "lucide-react";
 import { useState } from "react";
+import { CreateActiviteModal } from "./create-activite-modal";
+import { LocaleDateModal } from "./locale-date-modal";
 
 export function TripDetailsPage(){
     const [isCreatyActivityModalOpen, setIsCreatyActivityModalOpen] = useState<boolean>(false);
@@ -34,32 +36,7 @@ export function TripDetailsPage(){
 
     return(
         <div className="max-w-6xl px-6 py-10 mx-auto space-y-8">
-            <div className="flex items-center justify-between bg-zinc-900 px-4 h-16 rounded-xl shadow-shape">
-                <div className="flex items-center gap-2">
-                    <MapPin className="size-5 text-zinc-400"/>
-
-                    <span className="text-zinc-100">
-                        Rio de Janeiro, Brasil
-                    </span>
-                </div>
-                
-                <div className="flex items-center gap-5">
-                    <div className="flex items-center gap-2">
-                        <Calendar className="size-5 text-zinc-400"/>
-
-                        <span className="text-zinc-100">
-                            17 a 23 de Agosto
-                        </span>
-                    </div>
-
-                    <div className="w-px h-6 bg-zinc-800"/>
-
-                    <button className="bg-zinc-800 text-zinc-200 flex items-center gap-2 px-5 py-2 rounded-lg font-medium hover:bg-zinc-700">
-                        Alterar local/data
-                        <Settings2 className="size-5"/>
-                    </button>
-                </div>
-            </div>
+            <LocaleDateModal/>
 
             <main className="flex gap-16 px-4">
                 <div className="flex-1 space-y-6">
@@ -181,68 +158,9 @@ export function TripDetailsPage(){
             </main>
 
             { isCreatyActivityModalOpen && (
-                <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
-                    <div className="flex flex-col items-center bg-zinc-900 w-[540px] rounded-xl text-zinc-400 px-6 py-5 shadow-shape">
-                        <div className="flex flex-col justify-center w-full gap-5">
-                            <div className="space-y-2">
-                                <div className="flex items-center justify-between">
-                                    <h2 className="text-zinc-50 text-lg font-semibold">
-                                        Cadastrar atividade
-                                    </h2>
-                                
-                                    <button>
-                                        <X className="size-5" onClick={closeCreatyActivityModal}/>
-                                    </button>
-                                </div>
-                                
-                                <p className="text-zinc-400 text-sm">
-                                    Todos convidados podem visualizar as atividades.
-                                </p>
-                            </div>
-                        
-                            <form className="space-y-3 w-full">
-                                <div className="flex flex-col items-center gap-2 w-full">
-                                    
-                                    <div className="flex items-center gap-2 w-full h-14 bg-zinc-950 px-3 py-2 rounded-lg">
-                                        <Tag className="size-5 text-zinc-400"/>
-                                    
-                                        <input type="text"
-                                            name="nameUserInput"
-                                            id="nameUserInput"
-                                            className="bg-transparent placeholder-zinc-400 text-lg outline-none w-full text-zinc-100"
-                                            placeholder="Qual a atividade?"/>
-                                    </div>
-                    
-                                    <div className="flex items-center gap-2 w-full">
-                                        <div className="bg-zinc-950 flex flex-1 items-center h-14 py-4 px-2.5 gap-2.5 rounded-lg">
-                                            <Calendar className="size-5 text-zinc-400"/>
-
-                                            <input type="date"
-                                                name="dateInput"
-                                                id="dateInput"
-                                                className="bg-transparent placeholder-zinc-400 text-lg outline-none w-full text-zinc-100"
-                                                placeholder="Data"/>
-                                        </div>
-
-                                        <div className="bg-zinc-950 flex items-center w-[140px] h-14 py-4 px-2.5 gap-2.5 rounded-lg">
-                                            <Clock className="size-5 text-zinc-400 shrink-0"/>
-
-                                            <input type="time"
-                                                name="clockInput"
-                                                id="clockInput"
-                                                className="bg-transparent placeholder-zinc-400 text-lg outline-none text-zinc-100"
-                                                placeholder="Horário"/>
-                                        </div>
-                                    </div>
-                                </div>
-                
-                                <button className="bg-lime-300 text-lime-950 flex items-center justify-center px-5 py-2 gap-2 rounded-lg font-medium hover:bg-lime-400 w-full">
-                                    Salvar atividade
-                                </button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
+                <CreateActiviteModal
+                    closeCreatyActivityModal={closeCreatyActivityModal}
+                />
             )}
 
             { isLinkRegistrationModalOpen && (
