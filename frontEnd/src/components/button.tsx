a2791e151af68678b6ffd5b7ccd65656cd682201
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-08-10 13:09:20 -0300

    adicao da variavel de tamanho para o butao.

--- frontEnd/src/components/button.tsx
+++ frontEnd/src/components/button.tsx
@@ -1,26 +1,31 @@
-import { ComponentProps, ReactNode } from "react";
 import { tv, VariantProps } from "tailwind-variants"; //tv -> abreviação para tailwind-variants.
+import { ComponentProps, ReactNode } from "react";
 
 const buttonVariants = tv({
-    base: "flex items-center gap-2 px-5 py-2 rounded-lg font-medium",
+    base: "flex items-center justify-center gap-2 px-5 rounded-lg font-medium",
     variants:{
         colors:{
             primary: "bg-lime-300 text-lime-950 hover:bg-lime-400",
             secundary: "bg-zinc-800 text-zinc-200 hover:bg-zinc-700"
+        },
+        size:{
+            default: "py-2",
+            full: "w-full h-11"
         }
     },
     defaultVariants:{
-        colors: "primary"
+        colors: "primary",
+        size: "default"
     }
-})
+});
 
 interface ButtonType extends ComponentProps<"button">, VariantProps<typeof buttonVariants>{
     children: ReactNode;
 }
 
-export function Button({children, colors, ...props}: Readonly<ButtonType>){
+export function Button({children, colors, size, ...props}: Readonly<ButtonType>){
     return(
-        <button {...props} className={buttonVariants({colors})}>
+        <button {...props} className={buttonVariants({colors, size})}>
             {children}
         </button>
     )
