f4360ad51927e7d0d8d13b39c21d81e136564954
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-08-12 20:55:51 -0300

    instalacao do fastify-type-provider-zod, para tornar mais segura a aplicacao e a chamada da rona no servidor.

--- backEnd/src/server.ts
+++ backEnd/src/server.ts
@@ -1,6 +1,8 @@
+import { createTrip } from "./routs/create-trip";
 import fastify from "fastify";
 
 const app = fastify();
+app.register(createTrip);
 
 app.listen({port: 3333}).then(() => {
     console.log("Server running");
