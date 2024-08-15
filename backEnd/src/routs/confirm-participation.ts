6b795934454b86aaa689b45152531dfcc9cf42aa
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-08-15 12:03:47 -0300

    criacao da rota de confirmacao de participacao na viagem, e sua adicao na api.

---
+++ backEnd/src/routs/confirm-participation.ts
@@ -0,0 +1,48 @@
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { FastifyInstance } from "fastify";
+import { prisma } from "../lib/prisma";
+import { z } from "zod";
+
+export async function confirmParticipation(app: FastifyInstance){
+    app.withTypeProvider<ZodTypeProvider>().get("/trips/:tripId/confirm/:participantId", {
+        schema: {
+            params: z.object({
+                tripId: z.string().uuid(),
+                participantId: z.string().uuid()
+            })
+        }
+    }, async (request, reply) => {
+        const {tripId, participantId} = request.params;
+
+        const trip = await prisma.trip.findUnique({
+            where: {
+                id: tripId
+            }
+        });
+
+        const participant = await prisma.participant.findUnique({
+            where: {
+                id: participantId,
+                trip_id: tripId
+            }
+        });
+
+        if(!trip?.is_confirmed){
+            return reply.redirect(`http://localhost:3333/trips/${tripId}/confirm`);
+        } else if(participant?.is_confirmed){
+            return reply.redirect(`http://localhost:3030/trips/${tripId}`);
+        }
+
+        await prisma.participant.update({
+            where: {
+                id: participantId,
+                trip_id: tripId
+            },
+            data: {
+                is_confirmed: true
+            }
+        });
+
+        return "Deu bom";
+    })
+}
--- backEnd/src/server.ts
+++ backEnd/src/server.ts
@@ -3,6 +3,7 @@ import { confirmTrip } from "./routs/confirm-trip";
 import { createTrip } from "./routs/create-trip";
 import cors from "@fastify/cors";
 import fastify from "fastify";
+import { confirmParticipation } from "./routs/confirm-participation";
 
 const app = fastify();
 
@@ -15,6 +16,7 @@ app.setValidatorCompiler(validatorCompiler);
 
 app.register(createTrip);
 app.register(confirmTrip);
+app.register(confirmParticipation);
 
 app.listen({port: 3333}).then(() => {
     console.log("Server running");
