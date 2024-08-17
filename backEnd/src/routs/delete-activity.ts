722e4429bf8de4483be0e8bd13b84811a9e18f14
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-08-17 16:38:35 -0300

    criacao e adicao da rota de deletar uma atividade.

---
+++ backEnd/src/routs/delete-activity.ts
@@ -0,0 +1,34 @@
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { FastifyInstance } from "fastify";
+import { prisma } from "../lib/prisma";
+import { z } from "zod";
+
+export async function deleteActivity(app: FastifyInstance){
+    app.withTypeProvider<ZodTypeProvider>().delete("/activities/:activityId/delete", {
+        schema: {
+            params: z.object({
+                activityId: z.string().uuid() 
+            })
+        }
+    }, async (request) => {
+        const activityId = request.params.activityId;
+
+        if(!activityId){
+            return "Activity not found.";
+        }
+
+        try{
+            await prisma.activity.delete({
+                where: {
+                    id: activityId
+                }
+            });
+            return "Activity delete succesfully.";
+        } catch(error){
+            console.error(`Erro: ${error}`);
+            return;
+        }
+
+        
+    })
+}
--- backEnd/src/server.ts
+++ backEnd/src/server.ts
@@ -1,5 +1,7 @@
 import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
 import { confirmParticipation } from "./routs/confirm-participation";
+import { getParticipants } from "./routs/get-participants";
+import { deleteActivity } from "./routs/delete-activity";
 import { createActivity } from "./routs/create-activity";
 import { getActivities } from "./routs/get-activities";
 import { confirmTrip } from "./routs/confirm-trip";
@@ -8,7 +10,6 @@ import { createLink } from "./routs/create-link";
 import { getLinks } from "./routs/get-links";
 import cors from "@fastify/cors";
 import fastify from "fastify";
-import { getParticipants } from "./routs/get-participants";
 
 const app = fastify();
 
@@ -23,8 +24,9 @@ app.register(getLinks);
 app.register(createTrip);
 app.register(createLink);
 app.register(confirmTrip);
-app. register(getActivities)
+app.register(getActivities);
 app.register(createActivity);
+app.register(deleteActivity);
 app.register(getParticipants);
 app.register(confirmParticipation);
 
