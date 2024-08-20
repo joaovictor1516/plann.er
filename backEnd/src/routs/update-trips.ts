95867a9537eff5b4cf4b6a16fae1058ecfd2959c
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-08-20 17:47:36 -0300

    refatoracao, adicao de validacao das datas da viagem.

--- backEnd/src/routs/update-trips.ts
+++ backEnd/src/routs/update-trips.ts
@@ -1,6 +1,7 @@
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { FastifyInstance } from "fastify";
 import { prisma } from "../lib/prisma";
+import { dayjs } from "../lib/dayjs";
 import { z } from "zod";
 
 export async function updateTrips(app: FastifyInstance){
@@ -18,6 +19,10 @@ export async function updateTrips(app: FastifyInstance){
     }, async (request) => {
         const tripId = request.params.tripId;
         const {destination, starts_at, ends_at} = request.body;
+
+        if(dayjs(ends_at).isBefore(starts_at) || dayjs(starts_at).isBefore(new Date())){
+            return "Invalite date";
+        }
 
         try{
             await prisma.trip.update({
