e9636f740f5eb7903e8cb56640b24c6713fe91d0
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-08-26 16:28:28 -0300

    adicao da url do front-end na envSchema.

--- backEnd/src/lib/envSchema.ts
+++ backEnd/src/lib/envSchema.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 const envSchema = z.object({
     DATA_BASE_URL: z.string().url(),
     API_BASE_URL: z.string().url(),
+    WEB_BASE_URL: z.string().url(),
     PORT: z.coerce.number().default(3333)
 });
 
