eee16af822447e1af7066c66d592616d4674db29
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-09-04 11:59:25 -0300

    criacao da funcao responsavel pela captura das informacoes da viagem, e localizacao das datas da vagem.

--- frontEnd/src/pages/trip-details/locale-date-modal.tsx
+++ frontEnd/src/pages/trip-details/locale-date-modal.tsx
@@ -1,11 +1,39 @@
 import { Calendar, MapPin, Settings2 } from "lucide-react";
 import { Button } from "../../components/button";
+import { api } from "../../lib/axios";
+import { useState } from "react";
 
 interface LocalDateModalType{
     changeDateTime: () => void;
 }
 
 export function LocaleDateModal(props: Readonly<LocalDateModalType>){
+    const [destination, setDestination] = useState<string>("");
+    const [startsAt, setStartsAt] = useState<string>("");
+    const [endsAt, setEndsAt] = useState<string>("");
+
+    const options: object = {
+                weekday: "long",
+                month: "long",
+                year: "numeric",
+                day: "numeric"
+            }
+    
+    function tackeDastinationAndDate(tripId: string): void{
+        api.get(`http://localhost:3333/trips/${tripId}/details`)
+        .then((response) => {
+            const tripDetails = response.data.trip;
+
+            setDestination(tripDetails.destination);
+            setStartsAt(new Date(tripDetails.starts_at).toLocaleDateString("pt-BR", options));
+            setEndsAt(new Date(tripDetails.ends_at).toLocaleDateString("pt-BR", options));
+        })
+        .catch((error) => {
+            console.error(error);
+        })
+    }
+
+   tackeDastinationAndDate("a698e129-04d4-48fd-a805-004be703ce60");
     return(
         <div className="space-x-4 bg-zinc-900">
             <div className="flex items-center justify-between bg-zinc-900 px-4 h-16 rounded-xl shadow-shape">
@@ -13,7 +41,7 @@ export function LocaleDateModal(props: Readonly<LocalDateModalType>){
                     <MapPin className="size-5 text-zinc-400"/>
 
                     <span className="text-zinc-100">
-                        Rio de Janeiro, Brasil
+                        {destination}
                     </span>
                 </div>
                 
@@ -22,7 +50,7 @@ export function LocaleDateModal(props: Readonly<LocalDateModalType>){
                         <Calendar className="size-5 text-zinc-400"/>
 
                         <span className="text-zinc-100">
-                            17 a 23 de Agosto
+                            {startsAt} até {endsAt}
                         </span>
                     </div>
 
