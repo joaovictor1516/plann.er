dc9157994590afaa838bf4175857e4b08fac5012
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-09-05 15:17:19 -0300

    implementacao de melhores praticas para a consulta das atividades cadastradas no bd.

--- frontEnd/src/pages/trip-details/activity-modal.tsx
+++ frontEnd/src/pages/trip-details/activity-modal.tsx
@@ -1,12 +1,14 @@
 import { Plus, CircleCheck } from "lucide-react";
 import { Button } from "../../components/button";
+import { ActivityInformations } from "../../lib/interfaces";
 
 interface ActivityModalType{
     openCreatyActivityModal: () => void;
-    takeActivities: (id: string) => void;
+    activityInformations: ActivityInformations[];
 }
 
 export function ActivityModal(props: Readonly<ActivityModalType>){
+
     return(<div className="flex-1 space-y-6">
         <div className="flex items-center justify-between">
             <h2 className="text-3xl font-semibold">
@@ -18,35 +20,34 @@ export function ActivityModal(props: Readonly<ActivityModalType>){
                 Cadastrar atividade
             </Button>
         </div>
-
+            
         <div className="space-y-8">
-            <div className="space-y-3">
-                <div className="flex gap-2 items-baseline">
-                    <span className="text-zinc-300 text-xl font-semibold">Dia 17</span>
-                    <span className="text-zinc-500 text-xs">Sábado</span>
-                </div>
-                
-                <p className="text-zinc-500 text-sm">Nenhuma atividade cadastrada nesta data.</p>
-            </div>
+            {props.activityInformations?.map((information) => {
+                return (
+                    <div className="space-y-3">
+                        <div className="flex gap-2 items-baseline">
+                            <span className="text-zinc-300 text-xl font-semibold">{information.date}</span>
+                            <span className="text-zinc-500 text-xs">{information.dateDayWeek}</span>
+                        </div>
 
-            <div className="space-y-3">
-                <div className="flex gap-2 items-baseline">
-                    <span className="text-zinc-300 text-xl font-semibold">Dia 18</span>
-                    <span className="text-zinc-500 text-xs">Domingo</span>
-                </div>
-
-                <div className="flex justify-between items-center gap-3 h-10 px-4 rounded-xl bg-zinc-900 text-zinc-100 shadow-shape">
-                    <CircleCheck className="size-5 text-lime-300"/>
-                    <span className="flex-1">Corrida de Kart</span>
-                    <p className="text-zinc-400 text-sm">14:00h</p>
-                </div>
-
-                <div className="flex justify-between items-center gap-3 h-10 px-4 rounded-xl bg-zinc-900 text-zinc-100 shadow-shape">
-                    <CircleCheck className="size-5 text-lime-300"/>
-                    <span className="flex-1">Jantar</span>
-                    <p className="text-zinc-400 text-sm">21:00h</p>
-                </div>
-            </div>
+                        {information.activities?.map((activity) => {
+                            return activity.hasActivity ? (
+                                <>
+                                    <div className="flex justify-between items-center gap-3 h-10 px-4 rounded-xl bg-zinc-900 text-zinc-100 shadow-shape">
+                                        <CircleCheck className="size-5 text-lime-300"/>
+                                        <span className="flex-1">{activity.title}</span>
+                                        <p className="text-zinc-400 text-sm">{activity.time}</p>
+                                    </div>
+                                </>
+                            ) : (
+                                <div className="space-y-3">
+                                    <p className="text-zinc-500 text-sm">Nenhuma atividade cadastrada nesta data.</p>
+                                </div> 
+                            )
+                        })}
+                    </div>
+                )
+            })}
         </div>
     </div>)
 }
