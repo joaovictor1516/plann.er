af4d2c6858c580b31083d27e23a9a67ca9a60b33
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-09-11 12:24:55 -0300

    adicao da funmcao de adicionar links importantes.

--- frontEnd/src/pages/trip-details/registration-link-modal.tsx
+++ frontEnd/src/pages/trip-details/registration-link-modal.tsx
@@ -1,8 +1,10 @@
-import { X, Tag, Link2 } from "lucide-react";
 import { Button } from "../../components/button";
+import { X, Tag, Link2 } from "lucide-react";
+import { FormEvent } from "react";
 
 interface RegistrationLinkModalType{
     closeLinkRegistrationModal: () => void;
+    createLink: (event: FormEvent<HTMLFormElement>) => void;
 }
 
 export function RegistrationLinkModal(props: Readonly<RegistrationLinkModalType>){
@@ -24,13 +26,13 @@ export function RegistrationLinkModal(props: Readonly<RegistrationLinkModalType>
                 </p>
                 </div>
                 
-                <form className="space-y-3 w-full">
+                <form onSubmit={props.createLink} className="space-y-3 w-full">
                     <div className="flex flex-col items-center gap-2 w-full">
                         <div className="flex items-center gap-2 w-full bg-zinc-950 px-3 py-2 rounded-lg">
                         <Tag className="size-5 text-zinc-400"/>
                         <input type="text"
-                                name="nameLinkInput"
-                                id="nameLinkInput"
+                                name="titleInput"
+                                id="titleInput"
                                 className="bg-transparent placeholder-zinc-400 text-lg outline-none w-full text-zinc-100"
                                 placeholder="Título do link"/>
                         </div>
@@ -45,7 +47,7 @@ export function RegistrationLinkModal(props: Readonly<RegistrationLinkModalType>
                         </div>
                     </div>
 
-                    <Button colors="primary" size="full">
+                    <Button type="submit" colors="primary" size="full">
                         Salvar link
                     </Button>
                 </form>
