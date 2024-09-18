b9179f7f2cc14db01cb9ca223d8437e896631826
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-09-18 17:48:45 -0300

    refatoracao: criacao de uma condicional para informar quando a viagem nao tem links cadastrados.

--- frontEnd/src/pages/trip-details/link-modal.tsx
+++ frontEnd/src/pages/trip-details/link-modal.tsx
@@ -11,37 +11,43 @@ interface LinkModalType {
 export function LinkModal(props: Readonly<LinkModalType>){
     return(
         <div className="space-y-6">
-                        <h2 className="text-xl font-semibold">Links importantes</h2>
-                        
-                        <div className="space-y-5">
-                            {props.links.map((link) => {
-                                return (
-                                    <div key={link.id} className="flex flex-col gap-1">
-                                        <div className="text-zinc-400 flex items-center justify-between">
-                                            <div className="space-y-1.5 flex-1">
-                                                <span className="text-zinc-100 block text-lg font-semibold">
-                                                    {link.title}
-                                                </span>
-                                            
-                                                <a href={link.url} className="w-60 h-[17px] block truncate text-base hover:text-zinc-200">
-                                                    {link.url}
-                                                </a>
-                                            </div>
-                                            <Link2 className="size-5"/>
-                                        </div>
-
-                                        <Button color="primary" size="full" onClick={() => {props.deleteLink(link.id)}}>
-                                            deletar
-                                        </Button>
+            <h2 className="text-xl font-semibold">Links importantes</h2>
+            
+            <div className="space-y-5">
+                {props.links !== undefined ? 
+                    props.links.map((link) => {
+                        return (
+                            <div key={link.id} className="flex flex-col gap-1">
+                                <div className="text-zinc-400 flex items-center justify-between">
+                                    <div className="space-y-1.5 flex-1">
+                                        <span className="text-zinc-100 block text-lg font-semibold">
+                                            {link.title}
+                                        </span>
+                                    
+                                        <a href={link.url} className="w-60 h-[17px] block truncate text-base hover:text-zinc-200">
+                                            {link.url}
+                                        </a>
                                     </div>
-                                )
-                            })}
+                                    <Link2 className="size-5"/>
+                                </div>
 
-                            <Button colors="secundary" size="full" onClick={props.openLinkRegistrationModal}>
-                                <Plus className=""/>
-                                Cadastrar novo link
-                            </Button>
-                        </div>
-                    </div>
+                                <Button color="primary" size="full" onClick={() => {props.deleteLink(link.id)}}>
+                                    deletar
+                                </Button>
+                            </div>
+                        )
+                    }) : (
+                        <span className="text-zinc-100 block text-lg font-semibold">
+                            Nenhum link cadastrado
+                        </span>
+                    )
+                }
+
+                <Button colors="secundary" size="full" onClick={props.openLinkRegistrationModal}>
+                    <Plus className=""/>
+                    Cadastrar novo link
+                </Button>
+            </div>
+        </div>
     )
 }
