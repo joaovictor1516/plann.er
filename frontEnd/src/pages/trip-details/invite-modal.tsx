6a775a2f76b4c5e607341ecb5163733febf66c1c
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-09-18 17:45:32 -0300

    refatoracao: adicao de uma condicional para mostrar uma mensagem quando a viagem nao tem convidados.

--- frontEnd/src/pages/trip-details/invite-modal.tsx
+++ frontEnd/src/pages/trip-details/invite-modal.tsx
@@ -14,27 +14,34 @@ export function InviteModal(props: Readonly<InviteModalType>){
             
             <div className="space-y-5">
                 
-                {props.invites.map((invite) => {
-                    return (
-                        <div key={invite.id} className="text-zinc-400">
-                            {invite.name ? (
-                                <span className="text-zinc-100 font-semibold">{invite.name}</span>
-                            ) : (
-                                <span className="text-zinc-100 font-semibold">Participante sem a confirmação do nome</span>
-                            )}
-
-                            {invite.is_confirmed ? (
-                                <CircleCheck className="size-5 ml-auto mr-0 text-lime-300"/>
+                {props.invites !== undefined ?
+                    props.invites.map((invite) => {
+                        return (
+                            <div key={invite.id} className="text-zinc-400">
+                                {invite.name ? (
+                                    <span className="text-zinc-100 font-semibold">{invite.name}</span>
                                 ) : (
-                                <CircleDashed className="size-5 ml-auto mr-0"/>
-                            )}
-                            
-                            <p className="w-60 h-[17px] truncate text-xs">
-                                {invite.email}
-                            </p>
-                        </div>
+                                    <span className="text-zinc-100 font-semibold">Participante sem a confirmação do nome</span>
+                                )}
+
+                                {invite.is_confirmed ? (
+                                    <CircleCheck className="size-5 ml-auto mr-0 text-lime-300"/>
+                                    ) : (
+                                    <CircleDashed className="size-5 ml-auto mr-0"/>
+                                )}
+                                
+                                <p className="w-60 h-[17px] truncate text-xs">
+                                    {invite.email}
+                                </p>
+                            </div>
+                        )
+                    })
+                    : (
+                         <div className="text-zinc-400">
+                                    <span className="text-zinc-100 font-semibold">Nenhum convite enviado</span>
+                            </div>
                     )
-                })}
+                }
 
                 <Button colors="secundary" size="full" onClick={props.openConfirmeInviteModal}>
                     <UserCog className=""/>
