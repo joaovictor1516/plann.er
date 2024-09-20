5cc15cd02abbb8f14b9ce721cece2f38417f143b
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-09-20 18:23:19 -0300

    inicio da criacao da interface de atualizar as atividades.

--- frontEnd/src/pages/trip-details/activity-modal.tsx
+++ frontEnd/src/pages/trip-details/activity-modal.tsx
@@ -1,6 +1,7 @@
-import { Plus, CircleCheck, CircleDashed, Trash, Pencil, Check } from "lucide-react";
+import { Plus, CircleCheck, CircleDashed, Trash, Pencil, Check, X } from "lucide-react";
 import { ActivityInformations } from "../../lib/interfaces";
 import { Button } from "../../components/button";
+import { useState } from "react";
 
 interface ActivityModalType{
     openCreatyActivityModal: () => void;
@@ -11,70 +12,94 @@ interface ActivityModalType{
 }
 
 export function ActivityModal(props: Readonly<ActivityModalType>){
+    const [isUpdateModalOpen, setIsUpdateModalOpen] = useState<boolean>(false);
 
-    return(<div className="flex-1 space-y-6">
-        <div className="flex items-center justify-between">
-            <h2 className="text-3xl font-semibold">
-                Atividades
-            </h2>
+    function openUpdateModal(){
+        setIsUpdateModalOpen(true);
+    }
 
-            <Button colors="primary" size="default" onClick={props.openCreatyActivityModal}>
-                <Plus className="size-5"/>
-                Cadastrar atividade
-            </Button>
-        </div>
-            
-        <div className="space-y-8">
-            {props.activityInformations?.map((information, index) => {
-                return (
-                    <div key={index} className="space-y-3">
-                        <div className="flex gap-2 items-baseline">
-                            <span className="text-zinc-300 text-xl font-semibold">{information.date}</span>
-                            <span className="text-zinc-500 text-xs">{information.dateDayWeek}</span>
+    function closeUpdateModal(){
+        setIsUpdateModalOpen(false);
+    }
+
+    return(
+        <div className="flex-1 space-y-6">
+            <div className="flex items-center justify-between">
+                <h2 className="text-3xl font-semibold">
+                    Atividades
+                </h2>
+
+                <Button colors="primary" size="default" onClick={props.openCreatyActivityModal}>
+                    <Plus className="size-5"/>
+                    Cadastrar atividade
+                </Button>
+            </div>
+                
+            <div className="space-y-8">
+                {props.activityInformations?.map((information, index) => {
+                    return (
+                        <div key={index} className="space-y-3">
+                            <div className="flex gap-2 items-baseline">
+                                <span className="text-zinc-300 text-xl font-semibold">{information.date}</span>
+                                <span className="text-zinc-500 text-xs">{information.dateDayWeek}</span>
+                            </div>
+
+                            {information.activities ? (
+                                    information.activities.map((activity) => {
+                                        return (
+                                            <div key={activity.id} className="flex justify-between items-center gap-3 h-12 px-4 rounded-xl bg-zinc-900 text-zinc-100 shadow-shape">
+                                                {activity.is_complited ? (
+                                                    <CircleCheck className="size-5 text-lime-300"/>
+                                                ) : (
+                                                    <CircleDashed className="size-5 text-zinc-400"/>
+                                                )}
+
+                                                <span className="flex-1">{activity.title}</span>
+                                                <p className="text-zinc-400 text-sm">{activity.occurs_at}</p>
+
+                                                {activity.is_complited ? (
+                                                    <Button colors="secundary" size="default">
+                                                        <Check className="size-5"/>
+                                                    </Button>
+                                                ) : (
+                                                    <Button colors="primary" size="default" onClick={() => props.completeActivity(activity.id)}>
+                                                        <Check className="size-5"/>
+                                                    </Button>
+                                                )}
+
+                                                <Button colors="primary" size="default" onClick={openUpdateModal}>
+                                                    <Pencil className="size-5"/>
+                                                </Button>
+
+                                                <Button colors="primary" size="default" onClick={() => props.deleteActivity(activity.id)}>
+                                                    <Trash className="size-5"/>
+                                                </Button>
+
+                                                {isUpdateModalOpen && (
+                                                    <div className="bg-black">
+                                                        <div className="">
+                                                            <Button colors="secundary" onClick={closeUpdateModal}>
+                                                                <X className="size-5"/>
+                                                            </Button>
+                                                        </div>
+
+                                                        <form className="">
+                                                            
+                                                        </form>
+                                                    </div>
+                                                )}
+                                            </div>
+                                        )
+                                    })
+                                ) : (
+                                    <div className="space-y-3">
+                                        <p className="text-zinc-500 text-sm">Nenhuma atividade cadastrada nesta data.</p>
+                                    </div> 
+                                )
+                            }
                         </div>
-
-                        {information.activities ? (
-                                information.activities.map((activity) => {
-                                    return (
-                                        <div key={activity.id} className="flex justify-between items-center gap-3 h-12 px-4 rounded-xl bg-zinc-900 text-zinc-100 shadow-shape">
-                                            {activity.is_complited ? (
-                                                <CircleCheck className="size-5 text-lime-300"/>
-                                            ) : (
-                                                <CircleDashed className="size-5 text-zinc-400"/>
-                                            )}
-
-                                            <span className="flex-1">{activity.title}</span>
-                                            <p className="text-zinc-400 text-sm">{activity.occurs_at}</p>
-
-                                            {activity.is_complited ? (
-                                                <Button colors="secundary" size="default">
-                                                    <Check className="size-5"/>
-                                                </Button>
-                                            ) : (
-                                                <Button colors="primary" size="default" onClick={() => props.completeActivity(activity.id)}>
-                                                    <Check className="size-5"/>
-                                                </Button>
-                                            )}
-
-                                            <Button colors="primary" size="default" onClick={() => props.updateActivity(activity.id)}>
-                                                <Pencil className="size-5"/>
-                                            </Button>
-
-                                            <Button colors="primary" size="default" onClick={() => props.deleteActivity(activity.id)}>
-                                                <Trash className="size-5"/>
-                                            </Button>
-                                        </div>
-                                    )
-                                })
-                            ) : (
-                                <div className="space-y-3">
-                                    <p className="text-zinc-500 text-sm">Nenhuma atividade cadastrada nesta data.</p>
-                                </div> 
-                            )
-                        }
-                    </div>
-                )
-            })}
+                    )
+                })}
+            </div>
         </div>
-    </div>)
-}
+    )}
