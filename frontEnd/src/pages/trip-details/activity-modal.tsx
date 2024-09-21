8156d7d6e624c5d24156c312effe172b71503667
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-09-21 18:27:21 -0300

    refatoracao: remocao da tela de atualizacao da atividade por conta do tamanho da aplicacao.

--- frontEnd/src/pages/trip-details/activity-modal.tsx
+++ frontEnd/src/pages/trip-details/activity-modal.tsx
@@ -1,27 +1,16 @@
-import { Plus, CircleCheck, CircleDashed, Trash, Pencil, Check, X } from "lucide-react";
+import { Plus, CircleCheck, CircleDashed, Trash, Pencil, Check } from "lucide-react";
 import { ActivityInformations } from "../../lib/interfaces";
 import { Button } from "../../components/button";
-import { useState } from "react";
 
 interface ActivityModalType{
     openCreatyActivityModal: () => void;
+    openUpdateActivityModal: () => void;
     deleteActivity: (activityId: string) => void;
-    updateActivity: (ActivityId: string) => void;
     completeActivity: (activityId: string) => void;
     activityInformations: ActivityInformations[];
 }
 
 export function ActivityModal(props: Readonly<ActivityModalType>){
-    const [isUpdateModalOpen, setIsUpdateModalOpen] = useState<boolean>(false);
-
-    function openUpdateModal(){
-        setIsUpdateModalOpen(true);
-    }
-
-    function closeUpdateModal(){
-        setIsUpdateModalOpen(false);
-    }
-
     return(
         <div className="flex-1 space-y-6">
             <div className="flex items-center justify-between">
@@ -52,9 +41,7 @@ export function ActivityModal(props: Readonly<ActivityModalType>){
                                                     <CircleCheck className="size-5 text-lime-300"/>
                                                 ) : (
                                                     <CircleDashed className="size-5 text-zinc-400"/>
-                                                )}
-
-                                                <span className="flex-1">{activity.title}</span>
+                                                )}                                                <span className="flex-1">{activity.title}</span>
                                                 <p className="text-zinc-400 text-sm">{activity.occurs_at}</p>
 
                                                 {activity.is_complited ? (
@@ -67,27 +54,13 @@ export function ActivityModal(props: Readonly<ActivityModalType>){
                                                     </Button>
                                                 )}
 
-                                                <Button colors="primary" size="default" onClick={openUpdateModal}>
+                                                <Button colors="primary" size="default" onClick={props.openUpdateActivityModal}>
                                                     <Pencil className="size-5"/>
                                                 </Button>
 
                                                 <Button colors="primary" size="default" onClick={() => props.deleteActivity(activity.id)}>
                                                     <Trash className="size-5"/>
                                                 </Button>
-
-                                                {isUpdateModalOpen && (
-                                                    <div className="bg-black">
-                                                        <div className="">
-                                                            <Button colors="secundary" onClick={closeUpdateModal}>
-                                                                <X className="size-5"/>
-                                                            </Button>
-                                                        </div>
-
-                                                        <form className="">
-                                                            
-                                                        </form>
-                                                    </div>
-                                                )}
                                             </div>
                                         )
                                     })
