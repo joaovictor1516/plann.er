ee149de4b8aacbbe5e7cddf62ececb3984662bd7
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-09-23 09:44:43 -0300

    criacao e adicao da rota de busca pela atividade especifica.

---
+++ backEnd/src/routs/get-activity.ts
@@ -0,0 +1,29 @@
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { BadRequest } from "../lib/clientError";
+import { FastifyInstance } from "fastify";
+import { prisma } from "../lib/prisma";
+import { z } from "zod";
+
+export function getActivity(app: FastifyInstance){
+    app.withTypeProvider<ZodTypeProvider>().get("/activities/:activityId", {
+        schema:{
+            params: z.object({
+                activityId: z.string().uuid()
+            })
+        }
+    }, async (request, reply) => {
+        const activityId = request.params.activityId;
+
+        const activity = await prisma.activity.findUnique({
+            where: {
+                id: activityId
+            }
+        });
+
+        if(!activity){
+           throw new BadRequest("Activity not founded.");
+        }
+
+        return reply.code(200).send({activity});
+    });
+}
